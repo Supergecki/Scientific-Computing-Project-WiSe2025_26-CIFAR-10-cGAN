52df02047c9ad2b960b481a234934e8fb80acf2b
Supergecki <46622920+Supergecki@users.noreply.github.com>
2026-02-03 17:19:53 +0100

    Implement config file loading in train.py
    Adds support for loading configurations from the config files. The content will be stored in a `config` dictionary whose data can be accessed via string indices. Also enables command line arguments and parsing thereof, currently `-h` for help on how to use it and the `-b` flag to switch to baseline config (default is improved config). Resolves #12

---
+++ training/train.py
@@ -0,0 +1,21 @@
+import argparse
+import yaml
+
+# parsing command line arguments, currently used for:
+# - choosing the config file (defaults to `improved_config.yaml`, use '-b' flag to use `baseline_config.yaml`
+parser = argparse.ArgumentParser(description="Run training loop of the CIFAR-10 cGAN")
+parser.add_argument("-b", "--baseline", action='store_true', default=False,
+                    help="use baseline_config.yaml instead of improved_config.yaml for configurations")
+
+args = parser.parse_args()
+
+# Choose the correct config file
+config_file = '../config/improved_config.yaml'
+if args.baseline:
+    config_file = '../config/baseline_config.yaml'
+
+# Load config file and save configs in 'config' dictionary
+with open(config_file, 'r') as file:
+    config = yaml.safe_load(file)
+
+# Now configs are accessible via the dictionary, e.g. config['model']['embed_dim'] to get embedding dimension
