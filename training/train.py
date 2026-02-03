1d9b86a8c97cf5eb1f16d83fa39ab34696e2e528
Supergecki <46622920+Supergecki@users.noreply.github.com>
2026-02-03 18:31:31 +0100

    Add custom config file specification
    Users can now specify a custom config file instead of the two already given (`baseline_config.yaml` and `improved_config.yaml`) by using the `-c` parameter and specifying the path to the config file to be used. Overrides `-b` if given.
    Resolves #20

--- training/train.py
+++ training/train.py
@@ -8,15 +8,20 @@ os.chdir('..')
 
 # parsing command line arguments, currently used for:
 # - choosing the config file (defaults to `improved_config.yaml`, use '-b' flag to use `baseline_config.yaml`
+#                             or '-c' flag to specify another config file using its path)
 parser = argparse.ArgumentParser(description="Run training loop of the CIFAR-10 cGAN")
 parser.add_argument("-b", "--baseline", action='store_true', default=False,
                     help="use baseline_config.yaml instead of improved_config.yaml for configurations")
+parser.add_argument("-c", "--config", action='store', default=None,
+                    help="use specified config file instead of improved_config.yaml for configurations; overrides -b")
 
 args = parser.parse_args()
 
 # Choose the correct config file
-if args.baseline:
 config_file = './config/improved_config.yaml'
+if args.config is not None:
+    config_file = args.config
+elif args.baseline:
     config_file = './config/baseline_config.yaml'
 
 # Load config file and save configs in 'config' dictionary
