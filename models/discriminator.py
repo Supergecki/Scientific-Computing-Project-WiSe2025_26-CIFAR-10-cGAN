42b20f589c5d8c25f0e34961f4283336ab874f7d
Supergecki <46622920+Supergecki@users.noreply.github.com>
2026-02-05 15:54:20 +0100

    Put dummy classes for Discriminator and Generator
    Put in dummy classes for both models to later create a unified cGAN. The dummy classes will be overridden by issues #4 and #5

---
+++ models/discriminator.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, channel_config, num_classes=10, image_size=32):
+        pass
+
+    def forward(self, x):
+        pass
+    
