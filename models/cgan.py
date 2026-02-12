d88e3b87c46dfd0c7de68d818ab9d8244ed64c65
Supergecki <46622920+Supergecki@users.noreply.github.com>
2026-02-12 11:03:46 +0100

    Implement basic saving and loading methods
    The model can now be saved and loaded from a given checkpoint file. Note that for #6 this has to be updated to also save and load trained Embeddings!

--- models/cgan.py
+++ models/cgan.py
@@ -1,3 +1,4 @@
+import torch
 from discriminator import Discriminator
 from generator import Generator
 
@@ -10,6 +11,10 @@ class CGAN:
         List[int], number of channels per convolutional layer in the generator.
     :param discriminator_channel_config:
         List[int], number of channels per convolutional layer in the discriminator.
+    :param generator_optimizer:
+        torch Optimizer to use for the Generator
+    :param discriminator_optimizer:
+        torch Optimizer to use for the Discriminator
     :param latent_dim:
         int, dimension of the generator's latent space.
     :param num_classes:
@@ -17,10 +22,18 @@ class CGAN:
     :param image_size:
         int, image size in pixels (image_size x image_size) (default: 32 for CIFAR-10)
     """
-    def __init__(self, generator_channel_config, discriminator_channel_config,
-                 latent_dim, num_classes=10, image_size=32):
+    def __init__(self, generator_channel_config, discriminator_channel_config, generator_optimizer,
+                 discriminator_optimizer, latent_dim, num_classes=10, image_size=32):
         self.generator = Generator(generator_channel_config, latent_dim, num_classes, image_size)
         self.discriminator = Discriminator(discriminator_channel_config, num_classes, image_size)
+
+        # save metrics in the class itself
+        self.epoch = 0
+        self.loss = None
+
+        # save optimizers
+        self.generator_optimizer = generator_optimizer
+        self.discriminator_optimizer = discriminator_optimizer
 
     def generate(self, z):
         """
@@ -43,3 +56,33 @@ class CGAN:
             scalar probability that x was a real image
         """
         return self.discriminator.forward(x)
+
+    def save(self, path):
+        """
+        Saves the whole model to a given path.
+        :param path:
+            file path to save the model to
+        :return:
+            None
+        """
+        torch.save({'generator_state_dict': self.generator.state_dict(),
+                    'discriminator_state_dict': self.discriminator.state_dict(),
+                    'generator_optimizer_state_dict': self.generator_optimizer.state_dict(),
+                    'discriminator_optimizer_state_dict': self.discriminator_optimizer.state_dict(),
+                    'epoch': self.epoch, 'loss': self.loss}, path)
+
+    def load(self, path):
+        """
+        Loads the whole model from a given checkpoint at a path.
+        :param path:
+            file path to load the model from
+        :return:
+            None (all attributes of the class will be changed in place)
+        """
+        checkpoint = torch.load(path, weights_only=True)
+        self.generator.load_state_dict(checkpoint['generator_state_dict'])
+        self.discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
+        self.generator_optimizer.load_state_dict(checkpoint['generator_optimizer_state_dict'])
+        self.discriminator_optimizer.load_state_dict(checkpoint['discriminator_optimizer_state_dict'])
+        self.epoch = checkpoint['epoch']
+        self.loss = checkpoint['loss']
